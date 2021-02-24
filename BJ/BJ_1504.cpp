b2465b31b40256d77e1e2e024c70234e0a9f6863
philos1234 <ohhyun1948@gmail.com>
2021-02-25 00:06:57 +0900

    02-25

--- BJ/BJ_1504.cpp
+++ BJ/BJ_1504.cpp
@@ -101,164 +101,79 @@ int main() {
 */
 
 
+//백준 1504 특정한 최대 경로
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <stack>
-#include <functional>
+#include <utility>
 #include <algorithm>
-using namespace std;
-
-
-const int MAX = 800 + 1;
-
+#include <queue>
+using namespace std; 
+vector<pair<int, int>>adj[801];
 const int INF = 987654321;
+int dist[801];
+bool visited[801];
+int N, E;
+vector<int> daik(int start,int end) {
+	vector<int> dist(end + 1, INF);
+	fill(visited, visited + N, false);
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>q;
+	dist[start] = 0;
 
- 
+	q.push({ 0,start }); //dist최소,정점 번호
+	while (!q.empty()) {
 
-int N, M;
+		int cur;
+		do {
+			cur = q.top().second;
+			q.pop();
+		} while (!q.empty() && visited[cur] == true);
 
-int trace[MAX]; //경로 파악 위해
+		if (visited[cur] == true)break;
+		visited[cur] = true;
 
-vector<pair<int, int>> graph[MAX];
+		for (pair<int, int> p : adj[cur]) {
+			int next = p.first;
+			if (dist[next] > dist[cur] + p.second) {
+				dist[next] = dist[cur] + p.second;
+				q.push({ dist[next],next });
 
- 
+			}
+		}
 
-vector<int> dijkstra(int start, int vertex)
 
-{
-
-        vector<int> distance(vertex, INF);
-
-        distance[start] = 0;
-
- 
-
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-        pq.push({ 0, start });
-
- 
-
-        while (!pq.empty())
-
-        {
-
-                 int cost = pq.top().first;
-
-                 int cur = pq.top().second;
-
-                 pq.pop();
-
- 
-
-                 if (distance[cur] < cost)
-
-                         continue;
-
- 
-
-                 for (int i = 0; i < graph[cur].size(); i++)
-
-                 {
-
-                         int neighbor = graph[cur][i].first;
-
-                         int neighborDistance = cost + graph[cur][i].second;
-
- 
-
-                         //업데이트할 때마다 trace도 업데이트
-
-                         if (distance[neighbor] > neighborDistance)
-
-                         {
-
-                                 distance[neighbor] = neighborDistance;
-
-                                 trace[neighbor] = cur;
-
-                                 pq.push({ neighborDistance, neighbor });
-
-                         }
-
-                 }
-
-        }
-
-        return distance;
+	}
+	return dist;
 
 }
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	
+	cin >> N >> E;
+	int a, b, c;
+	for (int i = 0; i < E; i++) {
+		cin >> a >> b >> c;
+		
+		adj[a - 1].push_back({ b - 1,c });
+		adj[b - 1].push_back({ a - 1,c });
 
- 
+	}
+	int v1, v2;
+	cin >> v1 >> v2;
 
-int main(void)
 
-{
 
-        ios_base::sync_with_stdio(0);
 
-        cin.tie(0);
 
-        cin >> N >> M;
+	vector<int> ret = daik(0, N - 1);
+	vector<int> tmp1 = daik(v1 - 1, N - 1);
+	vector<int> tmp2 = daik(v2 - 1, N - 1);
 
- 
 
-        N++;
-
- 
-
-        for (int i = 0; i < M; i++)
-
-        {
-
-                 int u, v, w;
-
-                 cin >> u >> v >> w;
-
- 
-
-                 //양방향
-
-                 graph[u].push_back({ v, w });
-
-                 graph[v].push_back({ u, w });
-
-        }
-
-       
-
-        int node1, node2;
-
-        cin >> node1 >> node2;
-
- 
-
-        vector<int> result = dijkstra(1, N);
-
-        vector<int> temp1 = dijkstra(node1, N);
-
-        vector<int> temp2 = dijkstra(node2, N);
-
-        //두 가지 경우
-
-        //1. 1 -> node1 -> node2 -> N
-
-        //2. 1 -> node2 -> node1 -> N
-
-        int answer = min({ result[node1] + temp1[node2] + temp2[N - 1], result[node2] + temp2[node1] + temp1[N - 1] });
-
-        if (answer >= INF || answer < 0)
-
-                 cout << -1 << "\n";
-
-        else
-
-                 cout << answer << "\n";
-
-        return 0;
+	int ans = min(ret[v1 - 1] + tmp1[v2 - 1] + tmp2[N - 1], ret[v2 - 1] + tmp2[v1 - 1] + tmp1[N - 1]);
+	
+	if (ans >= INF || ans < 0)cout << -1 << '\n';
+	else cout << ans << '\n';
+	return 0;
 
 }
-
-
 
